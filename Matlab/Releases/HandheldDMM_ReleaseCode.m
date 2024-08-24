3fb8c39b76f4289a2224734c7432b2564d1d67a3
Matthias Henker <matthias.henker@outlook.de>
2024-08-24 12:10:19 +0200

    correct VersionID of HandheldDMM release (refresh of last release 2022-08-10)

--- Matlab/Releases/HandheldDMM_ReleaseCode.m
+++ Matlab/Releases/HandheldDMM_ReleaseCode.m
@@ -22,7 +22,7 @@
 % some config
 
 ModuleName = 'HandheldDMM';  % name of class file
-VersionID  = '2.1.0_2022-08-10_refresh'; % should match name of tag in git (version control)
+VersionID  = '2.1.0_2024-08-24_refresh'; % should match name of tag in git (version control)
 %VersionID  = 'x.y.z';
 
 % copy released files also to Support directory?
