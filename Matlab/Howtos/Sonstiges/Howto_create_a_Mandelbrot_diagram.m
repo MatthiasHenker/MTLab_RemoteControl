4f471e93586dbc3edfc94a435aa3d68cca63d9ab
Matthias Henker <matthias.henker@htw-dresden.de>
2025-05-13 12:55:22 +0200

    extend code to plot Julia-sets alternatively

--- Matlab/Howtos/Sonstiges/Howto_create_a_Mandelbrot_diagram.m
+++ Matlab/Howtos/Sonstiges/Howto_create_a_Mandelbrot_diagram.m
@@ -8,30 +8,41 @@ tic
 
 % ---- config ------------------------------------------------------------------
 % Parameter for Mandelbrot-Set
-maxIter        = 100;         % # of iterations (50 .. 150)
-selectPlotArea = 1;           % adapt to your needs
+maxIter             = 100;  % # of iterations (50 .. 150)
+selectPlotArea      = 3;    % adapt to your needs (0, 1, or 2)
+Mandelbrot_notJulia = false; % true for Mandelbrot-Plot false for Julia-Plot
 
 switch selectPlotArea
     case 1    % smaller window (right hand side)
-        xRange  = linspace(-0.9, 0.55, 2000);  % X - plot range (real)
-        yRange  = linspace(-1.15, 0.0, 2000);  % Y - plot range (imaginay)
+        xRange  = linspace(-0.9 ,  0.55, 2000); % X - plot range (real)
+        yRange  = linspace(-1.15,  0.0 , 2000); % Y - plot range (imaginay)
     case 2    % smaller window (left hand side
-        xRange  = linspace(-1.5, -1.2, 2000);  % X - plot range (real)
-        yRange  = linspace(-0.3, 0, 2000);     % Y - plot range (imaginay)
+        xRange  = linspace(-1.5 , -1.2 , 2000); % X - plot range (real)
+        yRange  = linspace(-0.3 ,   0  , 2000); % Y - plot range (imaginay)
+    case 3
+        xRange  = linspace(-1.5 ,  1.5 , 2000); % X - plot range (real)
+        yRange  = linspace(-1.16,  1.16, 2000); % Y - plot range (imaginay)
     otherwise % default (full plot)
-        xRange  = linspace(-2.2, 1.6, 2000);   % X - plot range (real)
-        yRange  = linspace(-1.5, 1.5, 2000);   % Y - plot range (imaginay)
+        xRange  = linspace(-2.2 ,  1.6 , 2000); % X - plot range (real)
+        yRange  = linspace(-1.5 ,  1.5 , 2000); % Y - plot range (imaginay)
 end
 
 % ---- actual code -------------------------------------------------------------
 
 % create mesh grid for complex plane
 [X, Y] = meshgrid(xRange, yRange);
-% constant complex offset
-C = X + 1i*Y;
 
 % initialize matrices (size of X, Y, C, and Z are identical)
-Z = zeros(size(X));
+if Mandelbrot_notJulia
+    % Mandelbrot-plot
+    C = X + 1i*Y;
+    Z = zeros(size(X));
+else
+    % Julia-plot
+    %C = -0.52 + 1i* (-0.52); % try out different start values
+    C = -0.43 + 1i* (-0.61);
+    Z = X + 1i*Y;
+end
 k = zeros(size(X));
 
 % Mandelbrot-loop
